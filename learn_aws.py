025f37f278017761aa7a0e0ed1cfd9f904bc97c9
Brand <Or.Brand@emc.com>
2017-08-21 11:00:19 +0300

    With mongo

--- learn_aws.py
+++ learn_aws.py
@@ -4,6 +4,11 @@ import logging
 import os
 import sys
 
+from pymongo import MongoClient
+from pymongo.cursor import Cursor
+
+client = MongoClient('mongodb://127.0.0.1/')
+db = client.test
 
 #Add project root dir
 sys.path.append(os.pardir)
@@ -28,7 +33,8 @@ API_KEY = "AIzaSyDDCiGvAgO3z82SK0sGztj4C-ehj7Qc8SA"
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    data = db.data.find_one({'name' : 'object1'})
+    return render_template("index.html", data=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
